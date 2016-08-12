5a45d66f7ea1522dee006bac3f846275bc4c3cc4
vschnose <vschnose@wustl.edu>
2016-08-12 14:25:42 -0500

    Merge remote-tracking branch 'refs/remotes/carlson9/master'
    # Conflicts:
    #	Day1/words.py

--- ordinal.py
+++ ordinal.py
@@ -2,68 +2,15 @@
 # turn an integral into an ordinal
 
 
-def ordinalFunction(integer):
+def ordinalFunction(N):
 	try:
-		int(integer)
-	except ValueError:
-		print "Put in an integer."
-	else:
-		print "Here is my integer: " + str(integer)
-
-def print_integer(integer):
-	if type(integer)==int:
-		print "Here is my integer: " + str(integer)
-	else:
-		raise Exception, "This is not an integer"
-
-def print_integer(integer):
-	if type(integer)==int: 
-		return "Here is my integer: " + str(integer)
-	else:
-		raise TypeError, "Enter an integer!"
-				
-def print_integer(integer):
-	try:
-		if integer %1==0:
-			return "Here is my integer: " + str(integer)
-		else:
-			return "This has decimals!"
-	except:
-		raise TypeError, "Enter a number!"
-		
-def print_integer(integer):
-	try:
-		if integer %1==0:
-			print "Congratulations! You entered an integer!"
-		else:
-			raise
-	except:
-		raise TypeError, "This is not an integer!"
-	else:
-		return "Here is my integer: " + str(integer)
-
-
-def print_integer(integer):
-	try:
-		if integer %1==0:
-			print "Here is my integer: " + str(integer)
-		else:
-			raise Exception
+		N = int(N)
 	except TypeError:
-		print "Enter a number!"
-	except:
-		print "Integers can't have decimals!"
-
-				
-def print_integer(integer):
-	try:
-		if integer %1==0:
-			print "Congratulations! You entered an integer!"
-		else:
-			raise Exception
-	except TypeError:
-		raise TypeError, "Enter a number!"
-	except:
-		raise TypeError, "Integers can't have decimals!"	
-	else:
-		return "Here is my integer: " + str(integer)
+		print 'Put in an integer.'
+	N = str(N)
+	end = {'1':'st', '2':'nd', '3':'rd'}
+	if N[-2:-1] == '1': 
+		return 'Here is an ordinal: ' + N + 'th'
+	if N[-1] in end.keys(): 
+		return 'Here is an ordinal: ' + N + end[N[-1]]
+	else: return 'Here is an ordinal: ' + N + 'th'
